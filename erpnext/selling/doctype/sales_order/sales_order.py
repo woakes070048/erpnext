cdf098c1939b4811da003d663f475c0c1d4d4899
ljain112 <ljain112@gmail.com>
2024-11-19 12:38:34 +0530

    fix: disable conversion to user tz for sales order calender

--- erpnext/selling/doctype/sales_order/sales_order.py
+++ erpnext/selling/doctype/sales_order/sales_order.py
@@ -1247,7 +1247,10 @@ def get_events(start, end, filters=None):
 		""",
 		{"start": start, "end": end},
 		as_dict=True,
-		update={"allDay": 0},
+		update={
+			"allDay": 0,
+			"convertToUserTz": 0,
+		},
 	)
 	return data
 
