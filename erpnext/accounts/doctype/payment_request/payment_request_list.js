e1c4d6e1e666ee539caf746ed303003ac89f8024
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-11-23 14:47:31 +0530

    refactor: Used object to get payment request status indicator

--- erpnext/accounts/doctype/payment_request/payment_request_list.js
+++ erpnext/accounts/doctype/payment_request/payment_request_list.js
@@ -1,21 +1,16 @@
+const INDICATORS = {
+	"Partially Paid": "orange",
+	Cancelled: "red",
+	Draft: "gray",
+	Failed: "red",
+	Initiated: "green",
+	Paid: "blue",
+	Requested: "green",
+};
+
 frappe.listview_settings["Payment Request"] = {
 	add_fields: ["status"],
 	get_indicator: function (doc) {
-		if (doc.status == "Draft") {
-			return [__("Draft"), "gray", "status,=,Draft"];
-		}
-		if (doc.status == "Requested") {
-			return [__("Requested"), "green", "status,=,Requested"];
-		} else if (doc.status == "Initiated") {
-			return [__("Initiated"), "green", "status,=,Initiated"];
-		} else if (doc.status == "Partially Paid") {
-			return [__("Partially Paid"), "orange", "status,=,Partially Paid"];
-		} else if (doc.status == "Paid") {
-			return [__("Paid"), "blue", "status,=,Paid"];
-		} else if (doc.status == "Failed") {
-			return [__("Failed"), "red", "status,=,Failed"];
-		} else if (doc.status == "Cancelled") {
-			return [__("Cancelled"), "red", "status,=,Cancelled"];
-		}
+		return [__(doc.status), INDICATORS[doc.status] || "gray", `status,=,${doc.status}`];
 	},
 };
