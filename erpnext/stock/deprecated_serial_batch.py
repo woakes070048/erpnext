079b86044efce7bf04c94900cdde6b1119c53fb2
rohitwaghchaure <rohitw1991@gmail.com>
2024-12-24 09:06:37 +0530

    fix: posting_time issue (#44870)

--- erpnext/stock/deprecated_serial_batch.py
+++ erpnext/stock/deprecated_serial_batch.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 import frappe
 from frappe.query_builder.functions import CombineDatetime, Sum
-from frappe.utils import flt
+from frappe.utils import flt, nowtime
 from pypika import Order
 
 from erpnext.deprecation_dumpster import deprecated
@@ -131,7 +131,10 @@ class DeprecatedBatchNoValuation:
 		sle = frappe.qb.DocType("Stock Ledger Entry")
 
 		timestamp_condition = None
-		if self.sle.posting_date and self.sle.posting_time:
+		if self.sle.posting_date:
+			if self.sle.posting_time is None:
+				self.sle.posting_time = nowtime()
+
 			posting_datetime = get_combine_datetime(self.sle.posting_date, self.sle.posting_time)
 			if not self.sle.creation:
 				posting_datetime = posting_datetime + datetime.timedelta(milliseconds=1)
