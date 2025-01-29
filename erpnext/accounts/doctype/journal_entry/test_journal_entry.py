9f20854bd9da74412cb220ce2caa2e246b9cf169
Lakshit Jain <108322669+ljain112@users.noreply.github.com>
2025-01-29 12:16:11 +0530

    fix: get stock balance filtered by company for validating stock value in jv (#45549)
    * fix: get stock balance filtered by company for validating stock value in jv
    
    * test: error is raised  on validate

--- erpnext/accounts/doctype/journal_entry/test_journal_entry.py
+++ erpnext/accounts/doctype/journal_entry/test_journal_entry.py
@@ -154,10 +154,9 @@ class TestJournalEntry(IntegrationTestCase):
 				"credit_in_account_currency": 0 if diff > 0 else abs(diff),
 			},
 		)
-		jv.insert()
 
 		if account_bal == stock_bal:
-			self.assertRaises(StockAccountInvalidTransaction, jv.submit)
+			self.assertRaises(StockAccountInvalidTransaction, jv.save)
 			frappe.db.rollback()
 		else:
 			jv.submit()
