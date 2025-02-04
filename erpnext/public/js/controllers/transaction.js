bfc01441a073c9e5658c2723b76235f5e8ed237f
Ejaaz Khan <ejaaz@frappe.io>
2025-02-05 00:09:16 +0530

    refactor: remove log

--- erpnext/public/js/controllers/transaction.js
+++ erpnext/public/js/controllers/transaction.js
@@ -2518,8 +2518,6 @@ erpnext.TransactionController = class TransactionController extends erpnext.taxe
 	}
 
 	payment_terms_template() {
-		console.log("payment_terms_template outside");
-
 		var me = this;
 		const doc = this.frm.doc;
 		if(doc.payment_terms_template && doc.doctype !== 'Delivery Note' && !doc.is_return) {
