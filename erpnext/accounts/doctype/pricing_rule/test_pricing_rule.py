ac3259b8f156131e2d5ab27e411f9d735f835626
Mihir Kandoi <kandoimihir@gmail.com>
2025-02-05 21:34:19 +0530

    test: added test

--- erpnext/accounts/doctype/pricing_rule/test_pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/test_pricing_rule.py
@@ -396,7 +396,6 @@ class TestPricingRule(IntegrationTestCase):
 			"price_or_product_discount": "Product",
 			"same_item": 1,
 			"free_qty": 1,
-			"enforce_free_item_qty": 1,
 			"company": "_Test Company",
 		}
 		frappe.get_doc(test_record.copy()).insert()
@@ -429,7 +428,6 @@ class TestPricingRule(IntegrationTestCase):
 			"same_item": 0,
 			"free_item": "_Test Item 2",
 			"free_qty": 1,
-			"enforce_free_item_qty": 1,
 			"company": "_Test Company",
 		}
 		frappe.get_doc(test_record.copy()).insert()
@@ -439,6 +437,54 @@ class TestPricingRule(IntegrationTestCase):
 		so.load_from_db()
 		self.assertEqual(so.items[1].is_free_item, 1)
 		self.assertEqual(so.items[1].item_code, "_Test Item 2")
+
+	def test_enforce_free_item_qty(self):
+		# this test is only for testing non-enforcement as all other tests in this file already test with enforcement
+		frappe.delete_doc_if_exists("Pricing Rule", "_Test Pricing Rule")
+		test_record = {
+			"doctype": "Pricing Rule",
+			"title": "_Test Pricing Rule",
+			"apply_on": "Item Code",
+			"currency": "USD",
+			"items": [
+				{
+					"item_code": "_Test Item",
+				}
+			],
+			"selling": 1,
+			"rate_or_discount": "Discount Percentage",
+			"rate": 0,
+			"min_qty": 0,
+			"max_qty": 7,
+			"discount_percentage": 17.5,
+			"price_or_product_discount": "Product",
+			"same_item": 0,
+			"free_item": "_Test Item 2",
+			"free_qty": 1,
+			"company": "_Test Company",
+		}
+		pricing_rule = frappe.get_doc(test_record.copy()).insert()
+
+		# With enforcement
+		so = make_sales_order(item_code="_Test Item", qty=1, do_not_submit=True)
+		self.assertEqual(so.items[1].is_free_item, 1)
+		self.assertEqual(so.items[1].item_code, "_Test Item 2")
+
+		# Test 1 : Saving a document with an item with pricing list without it's corresponding free item will cause it the free item to be refetched on save
+		so.items.pop(1)
+		so.save()
+		so.reload()
+		self.assertEqual(len(so.items), 2)
+
+		# Without enforcement
+		pricing_rule.enforce_free_item_qty = 0
+		pricing_rule.save()
+
+		# Test 2 : Deleted free item will not be fetched again on save without enfrocement
+		so.items.pop(1)
+		so.save()
+		so.reload()
+		self.assertEqual(len(so.items), 1)
 
 	def test_cumulative_pricing_rule(self):
 		frappe.delete_doc_if_exists("Pricing Rule", "_Test Cumulative Pricing Rule")
@@ -1123,7 +1169,6 @@ class TestPricingRule(IntegrationTestCase):
 			"price_or_product_discount": "Product",
 			"same_item": 1,
 			"free_qty": 1,
-			"enforce_free_item_qty": 1,
 			"round_free_qty": 1,
 			"is_recursive": 1,
 			"recurse_for": 2,
@@ -1169,7 +1214,6 @@ class TestPricingRule(IntegrationTestCase):
 			"price_or_product_discount": "Product",
 			"same_item": 1,
 			"free_qty": 10,
-			"enforce_free_item_qty": 1,
 			"round_free_qty": 1,
 			"is_recursive": 1,
 			"recurse_for": 100,
